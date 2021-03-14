887b01ebf262e0c90e5f82892de5ae06c750068a
Pedro Mota <00motapedro@gmail.com>
2021-03-14 23:27:16 +0000

    fixed main loop. added support for requests. added some books to the BD

--- client.erl
+++ client.erl
@@ -1,3 +1,24 @@
 -module(client).
 
--export([]).
+-export([create_account/5, delete_account/2,
+         request_book/3, return_book/3,
+         show_table/2]).
+
+create_account(Server, NrCC, Name, Address, Phone) ->
+  Server ! {add_person, self(), NrCC, Name, Address, Phone}.
+
+% Return all books hold by the person.
+delete_account(Server, NrCC) ->
+  Server ! {remove_person, self(), NrCC}.
+
+request_book(Server, ID, NrCC) ->
+  Server ! {add_request, self(), ID,  NrCC}.
+
+return_book(Server, ID, NrCC) ->
+  Server ! {remove_request, self(), ID,  NrCC}.
+
+show_table(Server, TableName) ->
+  Server ! {show_table, self(), TableName},
+  receive
+    Response -> Response
+  end.
--- server.erl
+++ server.erl
@@ -3,27 +3,54 @@
 -include_lib("stdlib/include/qlc.hrl").
 -import(lists, [foreach/2]).
 
--export([start/0, reset/0, show_table/1,
-         add_person/4, remove_person/1,
-         add_book/3, remove_book/1]).
+-export([setup/0, start/0, loop/0, reset/0,
+         show_table/1,
+         add_person/4, remove_person/1
+         ]).
 
 % person's primary key: nrCC
--record(person, {nrCC :: integer(), name :: string(), address :: string(), phone :: integer()}).
+-record(person, {nrCC, name, address, phone}).
 % book's primary key: id
--record(book, {id :: integer(), name :: string(), authors :: [string()]}).
+-record(book, {id, name, authors}).
+% request's primary key: {nrCC(person), id(book)}
+-record(request, {id, nrCC}).
 
-%% DB related
-start() ->
+
+bookshelf() -> [
+  {book, 1, "Programming Erlang: software for a concurrent world", ["Joe Armstrong"]},
+  {book, 2, "The little book of Semaphores", ["Allen B. Downey"]}
+  %{book, 3, "Numerical Otimization", ["Jorge Nocedal", "Stephen J. Wrigth"]},
+  %{book, 4, "A first course in mathematical modeling", ["Frank R. Giordano", "William P. Fox", "Steven B. Horton"]},
+  %{book, 5, "Convex Optimization", ["Stephen Boyd", "Lieven Vandenberghe"]}
+].
+
+setup() ->
     mnesia:create_schema([self()]),
     mnesia:start(),
     mnesia:create_table(person, [{attributes, record_info(fields, person)}]),
     mnesia:create_table(book, [{attributes, record_info(fields, book)}]),
-    %mnesia:create_table(request, [{attributes, record}])
-    mnesia:wait_for_tables([person, book], 20000).
+    mnesia:create_table(request, [{attributes, record_info(fields, request)}]),
+    mnesia:wait_for_tables([person, book, request], 20000),
+    mnesia:transaction(fun() -> foreach(fun mnesia:write/1, bookshelf()) end).
+
+start() -> spawn(fun() -> setup(), loop() end).
+
+loop() ->
+  receive
+    {show_table, From, Table} -> From ! show_table(Table), loop();
+
+    {add_person, _, NrCC, Name, Address, Phone} -> add_person(NrCC, Name, Address, Phone), loop();
+    {remove_person, _, NrCC} -> remove_person(NrCC), loop();
+
+    {add_request, _, ID, NrCC} -> add_request(ID, NrCC), loop();
+    {remove_request, _, ID, NrCC} -> remove_request(ID, NrCC), loop()
+  end.
 
 reset() ->
-  F = fun() -> [mnesia:delete(person, Key) || Key <- mnesia:all_keys(person)] end,
-  mnesia:transaction(F).
+  mnesia:clear_table(person),
+  mnesia:clear_table(book),
+  mnesia:clear_table(bookshelf),
+  mnesia:transaction(fun() -> foreach(fun mnesia:write/1, bookshelf()) end).
 
 show_table(Table) ->
   do(qlc:q([X || X <- mnesia:table(Table)])).
@@ -37,8 +64,11 @@ remove_entry(OId) ->
   mnesia:transaction(fun() -> mnesia:delete(OId) end).
 
 do(Q) ->
-  {atomic, Result} = mnesia:transaction(fun() -> qlc:e(Q) end),
-  Result.
+  F = fun() -> qlc:e(Q) end,
+  case mnesia:transaction(F) of
+  {atomic, Val} -> Val;
+  {aborted, Reason} -> Reason
+  end.
 
 add_person(NrCC, Name, Address, Phone) ->
   add_entry(#person{nrCC=NrCC, name=Name, address=Address, phone=Phone}).
@@ -47,9 +77,17 @@ remove_person(NrCC) ->
   OId = {person, NrCC},
   remove_entry(OId).
 
-add_book(ID, Name, Authors) ->
-  add_entry(#book{id=ID, name=Name, authors=Authors}).
+%
+%add_book({ID, Name, Authors}) ->
+%  add_entry(#book{id=ID, name=Name, authors=Authors}).
 
-remove_book(ID) ->
-  OId = {book, ID},
+%remove_book(ID) ->
+%  OId = {book, ID},
+%  remove_entry(OId).
+
+add_request(ID, NrCC) ->
+  add_entry(#request{id=ID, nrCC=NrCC}).
+
+remove_request(ID, NrCC) ->
+  OId = {request, {ID, NrCC}},
   remove_entry(OId).
