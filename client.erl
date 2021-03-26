5b574935c523576c6179e404a2fa139ecedfc81c
Pedro Mota <00motapedro@gmail.com>
2021-03-26 17:35:26 +0000

    fixed io and some bugs

--- client.erl
+++ client.erl
@@ -8,58 +8,67 @@
 person_requests(Server, CC) ->
   Server ! {person_requests, self(), CC},
   receive
-    {aborted, Reason} -> error_message(Reason);
-    {atomic, Requests} -> "[System] IDs of the books requested by the person with CC number " ++ CC ++ ": " ++ lists:concat(lists:join(", ", Requests)) ++ "."
+    {Server, {aborted, Reason}} -> io:format(error_message(Reason));
+    {Server, {atomic, Requests}} -> case Requests of
+                                      [] -> io:format("[System] The person with CC number ~w has no requests made.~n", [CC]);
+                                      _  -> io:format("[System] IDs of the books requested by the person with CC number ~w: ~s.~n", [CC, lists:concat(lists:join(", ", Requests))])
+                                    end
   end.
 
 book_requests(Server, Title) ->
   Server ! {book_requests, self(), Title},
   receive
-    {aborted, Reason} -> error_message(Reason);
-    {atomic, CCs} -> "[System] CC number of the people who requested the book \"" ++ Title ++ "\": " ++ lists:concat(lists:join(", ", CCs)) ++ "."
+    {Server, {aborted, Reason}} -> io:format(error_message(Reason));
+    {Server, {atomic, CCs}} -> case CCs of
+                                [] -> io:format("[System] No one requested the book \"~s\".~n", [Title]);
+                                _  -> io:format("[System] CC numbers of the people who requested the book \"~s\": ~s.~n", [Title, lists:concat(lists:join(", ", CCs))])
+                               end
   end.
 
 book_is_requested(Server, ID) ->
   Server ! {book_is_requested, self(), ID},
   receive
-    {aborted, Reason} -> error_message(Reason);
-    {atomic, true} -> "[System] The book is requested.~n";
-    {atomic, false} -> "[System] The book isn't requested.~n"
+    {Server, {aborted, Reason}} -> io:format(error_message(Reason));
+    {Server, {atomic, true}} -> io:format("[System] The book is requested.~n");
+    {Server, {atomic, false}} -> io:format("[System] The book isn't requested.~n")
   end.
 
 book_ids(Server, Title) ->
   Server ! {book_ids, self(), Title},
   receive
-    {aborted, Reason} -> error_message(Reason);
-    {atomic, IDs} -> "[System] Available IDs of the book \"" ++ Title ++ "\": " ++ lists:concat(lists:join(", ", IDs)) ++ "."
+    {Server, {aborted, Reason}} -> io:format(error_message(Reason));
+    {Server, {atomic, IDs}} -> io:format("[System] Available IDs of the book \"~s\": ~s.~n", [Title, lists:concat(lists:join(", ", IDs))])
   end.
 
 person_num_requests(Server, CC) ->
   Server ! {person_num_requests, self(), CC},
   receive
-    {aborted, Reason} -> error_message(Reason);
-    {atomic, Qt} -> "[System] The person with CC number " ++ CC ++ " has requested " ++ Qt ++ " books."
+    {Server, {aborted, Reason}} -> io:format(error_message(Reason));
+    {Server, {atomic, Qt}} -> case Qt of
+                                1 -> io:format("[System] The person with CC number ~w has requested 1 book.~n", [CC]);
+                                _ -> io:format("[System] The person with CC number ~w has requested ~w books.~n", [CC, Qt])
+                              end
   end.
 
 % Update methods
 request_book(Server, ID, CC) ->
   Server ! {add_request, self(), ID, CC},
   receive
-    {aborted, Reason} -> error_message(Reason);
-    {atomic} -> "[System] The book was requested with sucess."
+    {Server, {aborted, Reason}} -> io:format(error_message(Reason));
+    {Server, {atomic}} -> io:format("[System] The book was requested with sucess.~n")
   end.
 
 return_book(Server, ID, CC) ->
   Server ! {remove_request, self(), ID, CC},
   receive
-    {aborted, Reason} -> error_message(Reason);
-    {atomic} -> "[System] The book was returned with sucess."
+    {Server, {aborted, Reason}} -> io:format(error_message(Reason));
+    {Server, {atomic}} -> io:format("[System] The book was returned with sucess.~n")
   end.
 
 error_message(Reason) ->
-  Map = #{invalid_book_title  => "there's no book in this library with that title.",
-          invalid_book_id     => "there's no book in this library with that ID.",
-          invalid_person_cc   => "there's no person in the system with that CC number.",
-          invalid_request     => "the book is already requested.",
-          invalid_return      => "you don't possess the book to return it."},
-  "[System] Operation failed: " ++ maps:get(Reason, Map).
+  Map = #{invalid_book_title  => "There's no book in this library with that title.",
+          invalid_book_id     => "There's no book in this library with that ID.",
+          invalid_person_cc   => "There's no person in the system with that CC number.",
+          invalid_request     => "The book is already requested.",
+          invalid_return      => "You don't possess the book to return it."},
+  "[System] " ++ maps:get(Reason, Map) ++ "~n".
--- server.erl
+++ server.erl
@@ -2,14 +2,27 @@
 
 -include_lib("stdlib/include/qlc.hrl").
 
--import(lists, [member/2, foreach/2]).
+-import(lists, [member/2, foreach/2, sort/1]).
 -import(utils, [empty_list/1, or_list/1]).
 
--export([start/0, setup/0, valid/2]).
+-export([start/0]).
 
--record(book, {id, title, authors}).
--record(person, {cc, name, address, phone}).
+-record(book,    {id, title, authors}).
+-record(person,  {cc, name, address, phone}).
 -record(request, {id, cc}).
+
+do(Q) ->
+  F = fun() -> qlc:e(Q) end,
+  case mnesia:transaction(F) of
+    {atomic, Result} -> Result;
+    {aborted, Reason} -> Reason
+  end.
+
+add_entry(Entry) -> % Entry :: {tableName, attributes}
+  mnesia:transaction(fun() -> mnesia:write(Entry) end).
+
+remove_entry(OId) -> % OId :: {tableName, PK}
+  mnesia:transaction(fun() -> mnesia:delete(OId) end).
 
 bd() -> [
   % Books
@@ -19,52 +32,64 @@ bd() -> [
   {book, 4, "Numerical Otimization", {"Jorge Nocedal", "Stephen J. Wrigth"}},
   {book, 5, "A first course in mathematical modeling", {"Frank R. Giordano", "William P. Fox", "Steven B. Horton"}},
   {book, 6, "Convex Optimization", {"Stephen Boyd", "Lieven Vandenberghe"}},
+  {book, 7, "Introduction to Automata Theory, Languages and Computations", {"John E. Hopcroft", "Rajeev Motwani", "Jeffrey D. Ullman"}},
+  {book, 8, "Introduction to Automata Theory, Languages and Computations", {"John E. Hopcroft", "Rajeev Motwani", "Jeffrey D. Ullman"}},
+  {book, 9, "Introduction to the Theory of Computation", {"Michael Sipser"}},
+  {book, 10, "Introduction to Operations Research", {"Frederich S. Hillier", "Gerald J. Lieberman"}},
+  {book, 11, "Convex Optimization", {"Stephen Boyd", "Lieven Vandenberghe"}},
+  {book, 12, "Convex Optimization", {"Stephen Boyd", "Lieven Vandenberghe"}},
+  {book, 13, "Deep Learning", {"Ian Goodfellow", "Yoshua Bengio", "Aaron Courville"}},
+  {book, 14, "A Computational Introduction to Number Theory and Algebra", {"Victor Shoup"}},
+  {book, 15, "Introduction to Automata Theory, Languages and Computations", {"John E. Hopcroft", "Rajeev Motwani", "Jeffrey D. Ullman"}},
 
   % People
-  {person, 10, "Tommy Shelby", "Birmingham", 1231812},
-  {person, 20, "Tyrion Lannister", "Casterly Rock", 2312321},
-  {person, 30, "The Professor", "Spain", 8978221},
-  {person, 40, "Dolores", "WestWorld", 7481262}
+  {person, 1000, "Tommy Shelby", "Birmingham", 1231812},
+  {person, 2000, "Tyrion Lannister", "Casterly Rock", 2312321},
+  {person, 3000, "Sergio Marquina", "Spain", 8978221},
+  {person, 4000, "Dolores", "WestWorld", 7481262},
+  {person, 5000, "Bernard Lowe", "WestWorld", 7481262},
+  {person, 6000, "John Snow", "Tower of Joy", 7481262},
+  {person, 7000, "Andrés de Fonollosa", "Spain", 7481262}
 ].
 
 setup() ->
-    mnesia:create_schema([self()]),
-    mnesia:start(),
-    mnesia:create_table(person, [{attributes, record_info(fields, person)}]),
-    mnesia:create_table(book, [{attributes, record_info(fields, book)}]),
-    mnesia:create_table(request, [{attributes, record_info(fields, request)}]),
-    mnesia:wait_for_tables([person, book, request], 20000),
-    mnesia:transaction(fun() -> foreach(fun mnesia:write/1, bd()) end).
+  mnesia:create_schema([self()]),
+  mnesia:start(),
+  mnesia:create_table(person, [{attributes, record_info(fields, person)}]),
+  mnesia:create_table(book, [{attributes, record_info(fields, book)}]),
+  mnesia:create_table(request, [{attributes, record_info(fields, request)}]),
+  mnesia:wait_for_tables([person, book, request], 20000),
+  mnesia:transaction(fun() -> foreach(fun mnesia:write/1, bd()) end).
+
 
 loop() ->
   receive
     % Lookup
-    {person_requests, From, CC}     -> From ! person_requests(CC), loop();
-    {book_requests, From, Title}    -> From ! book_requests(Title), loop();
-    {book_is_requested, From, ID}   -> From ! book_is_requested(ID), loop();
-    {book_ids, From, Title}         -> From ! book_ids(Title), loop();
-    {person_num_requests, From, CC} -> From ! person_num_requests(CC), loop();
+    {person_requests, From, CC}     -> From ! {self(), person_requests(CC)}, loop();
+    {book_requests, From, Title}    -> From ! {self(), book_requests(Title)}, loop();
+    {book_is_requested, From, ID}   -> From ! {self(), book_is_requested(ID)}, loop();
+    {book_ids, From, Title}         -> From ! {self(), book_ids(Title)}, loop();
+    {person_num_requests, From, CC} -> From ! {self(), person_num_requests(CC)}, loop();
 
     % Update
-    {add_request, From, ID, CC}     -> From ! add_request(ID, CC), loop();
-    {remove_request, From, ID, CC}  -> From ! remove_request(ID, CC), loop()
+    {add_request, From, ID, CC}     -> From ! {self(), add_request(ID, CC)}, loop();
+    {remove_request, From, ID, CC}  -> From ! {self(), remove_request(ID, CC)}, loop()
   end.
 
 start() -> spawn(fun() -> setup(), loop() end).
 
-
 person_requests(CC) ->
   case valid(person, CC) of
     false -> {aborted, invalid_person_cc};
-    true -> Requests = do(qlc:q([element(1, X#request.id) || X <- mnesia:table(request), element(2, X#request.id) == CC])), {atomic, Requests}
+    true -> Requests = do(qlc:q([X#request.id || X <- mnesia:table(request), X#request.cc == CC])), {atomic, sort(Requests)}
   end.
 
 book_requests(Title) ->
   case valid(book, Title) of
     false -> {aborted, invalid_book_title};
-    true -> IDs = do(qlc:q([X#book.id || X <- mnesia:table(book), X#book.title == Title])), % Get possible codes for the book with title 'Title'
+    true -> IDs = do(qlc:q([X#book.id || X <- mnesia:table(book), X#book.title == Title])),            % Get possible codes for the book with title 'Title'
             CCs = do(qlc:q([X#request.cc || X <- mnesia:table(request), member(X#request.id, IDs)])),  % Get every person that request one of this codes
-            {atomic, CCs}
+            {atomic, sort(CCs)}
   end.
 
 book_is_requested(ID) ->
@@ -76,13 +101,13 @@ book_is_requested(ID) ->
 book_ids(Title) ->
   case valid(book, Title) of
     false -> {aborted, invalid_book_title};
-    true -> IDs = do(qlc:q([X#book.id || X <- mnesia:table(book), X#book.title == Title])), {atomic, IDs}
+    true -> IDs = do(qlc:q([X#book.id || X <- mnesia:table(book), X#book.title == Title])), {atomic, sort(IDs)}
   end.
 
 person_num_requests(CC) ->
   case valid(person, CC) of
     false -> {aborted, invalid_person_cc};
-    true -> Qt = length(person_requests(CC)), {atomic, Qt}
+    true -> {atomic, Requests} = person_requests(CC), Qt = length(Requests), {atomic, Qt}
   end.
 
 add_request(ID, CC) ->
@@ -102,26 +127,12 @@ remove_request(ID, CC) ->
     false -> {aborted, invalid_book_id};
     true -> case valid(person, CC) of
               false -> {aborted, invalid_person_cc};
-              true -> case valid(request, {ID, CC}) of 
+              true -> case valid(request, {ID, CC}) of
                         false -> {aborted, invalid_return};
-                        true -> remove_entry(#request{id=ID, cc=CC}), {atomic}
+                        true -> remove_entry({request, ID}), {atomic}
                       end
             end
   end.
-
-
-do(Q) ->
-  F = fun() -> qlc:e(Q) end,
-  case mnesia:transaction(F) of
-    {atomic, Result} -> Result;
-    {aborted, Reason} -> Reason
-  end.
-
-add_entry(Entry) -> % Entry :: {tableName, attributes}
-  mnesia:transaction(fun() -> mnesia:write(Entry) end).
-
-remove_entry(OId) -> % OId :: {tableName, PK}
-  mnesia:transaction(fun() -> mnesia:delete(OId) end).
 
 valid(Entity, Value) ->
   NotEmptyList = fun([]) -> false; (_) -> true end,
